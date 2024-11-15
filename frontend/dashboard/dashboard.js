5e6baa7b6493b4fef460e2eee66ad8a89ad25f33
Nicolasjeremy <nicolasjeremy11@gmail.com>
2024-11-15 13:51:12 +0700

    fix direction

--- frontend/dashboard/dashboard.js
+++ frontend/dashboard/dashboard.js
@@ -1,4 +1,4 @@
 document.getElementById("select-hero").addEventListener("click", () => {
     console.log("select-hero clicked");
-    window.location.href = "../hero-selection/hero.html"; // Ensure this path is correct
+    window.location.href = "StressDungeon//hero-selection/hero.html"; // Ensure this path is correct
 });
