0ce8e93c0a5509a969f2f4c4193374fd860e7815
Nicolasjeremy <nicolasjeremy11@gmail.com>
2024-11-15 20:39:05 +0700

    weqgf

--- game-backend/server.js
+++ game-backend/server.js
@@ -7,12 +7,13 @@ dotenv.config();
 
 // Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
-
+app.use(cors({
+  origin: 'https://nicolasjeremy.github.io/StressDungeon/' // Replace with your frontend domain
+}));
 // MongoDB connection
 const MONGO_URI = process.env.URI;
 mongoose
